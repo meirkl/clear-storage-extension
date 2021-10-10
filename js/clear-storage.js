39b75cdd0932cbe417cd3393998951d5aa01b535
Meir Keller <meirk@elementor.com>
2021-10-10 15:42:15 +0300

    fix delete cookies

--- js/clear-storage.js
+++ js/clear-storage.js
@@ -5,7 +5,10 @@ async function clearAndReload() {
         var cookies = document.cookie.split(';');
 
         for (var i = 0; i < cookies.length; i++) {
-            document.cookie = cookies[i] + '=;expires=' + new Date(0).toUTCString();
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf('=');
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + '=;expires=' + new Date(0).toUTCString();
         }
     }
 
